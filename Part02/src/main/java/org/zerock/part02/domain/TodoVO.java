803ee55c3278244364a4ff7ef456cbaf72c3792a
pso0207 <qkrtkddnjs0207@naver.com>
2025-07-28 23:44:20 +0900

    fuck!!!! git 잘못 만져서 다시 파일 다 날렸다.. 제발 디렉터리 여기저기 옮긴다고 막 건들지 말자

---
+++ Part02/src/main/java/org/zerock/part02/domain/TodoVO.java
@@ -0,0 +1,22 @@
+package org.zerock.part02.domain;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoVO {
+
+    private Long tno;
+
+    private String title;
+
+    private LocalDate dueDate;
+
+    private boolean finished;
+
+}
