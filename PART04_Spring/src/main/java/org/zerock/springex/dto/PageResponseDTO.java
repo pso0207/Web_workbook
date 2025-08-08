9c53c3eae072ef511cdd57767b562ed2baebd15e
pso0207 <qkrtkddnjs0207@naver.com>
2025-08-09 00:25:21 +0900

    Part04 끗

---
+++ PART04_Spring/src/main/java/org/zerock/springex/dto/PageResponseDTO.java
@@ -0,0 +1,54 @@
+package org.zerock.springex.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+public class PageResponseDTO<E> {
+
+    private int page;
+    private int size;
+    private int total;
+
+    //시작 페이지 번호
+    private int start;
+    //끝 페이지 번호
+    private int end;
+
+    //이전 페이지의 존재 여부
+    private boolean prev;
+    //다음 페이지 존재 여부
+    private boolean next;
+
+    private List<E> dtoList; // 나중에 다른 종류의 객체를 이용해서 DTO를 구성할 수 있도록 하기 위해서 제네릭 사용
+
+    @Builder(builderMethodName = "withAll")
+    public PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total){
+
+        this.page = pageRequestDTO.getPage();
+        this.size = pageRequestDTO.getSize();
+
+        this.total = total;
+        this.dtoList = dtoList;
+
+        this.end =   (int)(Math.ceil(this.page / 10.0 )) *  10;
+
+        this.start = this.end - 9;
+
+        int last =  (int)(Math.ceil((total/(double)size)));
+
+        this.end =  end > last ? last: end;
+
+        this.prev = this.start > 1;
+
+        this.next =  total > this.end * this.size;
+
+    }
+
+}
+
+
